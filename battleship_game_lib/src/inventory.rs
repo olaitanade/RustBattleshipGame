4efdbdedea7b0e00fb89e5b4331f7f2185dafa5d
Adetayo Olaitan <olaitan.adetayo@gmail.com>
2023-12-14 03:52:08 +0000

    :construction: wip

--- battleship_game_lib/src/inventory.rs
+++ battleship_game_lib/src/inventory.rs
@@ -18,8 +18,10 @@ pub enum ShipType {
 #[derive(Debug)]
 pub struct Square {
     pub origin: GridPoint,
-    pub ship: Ship
+    pub ship: Option<Ship>
 }
+
+
 
 #[derive(Debug)]
 pub struct GridPoint {
@@ -28,32 +30,40 @@ pub struct GridPoint {
 }
 
 #[derive(Debug)]
-struct Ship {
-    pub origin: GridPoint,
-    pub orientation: Orientation,
+pub struct Ship {
+    pub origin: Option<GridPoint>,
+    pub orientation: Option<Orientation>,
     size: i32,
     points: i32,
-    pub name: String,
-    pub ship_type: ShipType
+    name: String,
+    ship_type: ShipType
 }
 
 impl Ship {
-    pub fn build(ship_type: ShipType, origin: GridPoint, orientation: Orientation) -> Ship {
+    pub fn build(ship_type: ShipType) -> Ship {
         match ship_type {
-            ShipType::AircraftCarrier => Ship { ship_type, origin, orientation, size: 5, points: 2, name: String::from("Aircraft Carrier")},
-            ShipType::Battleship => Ship { ship_type, origin, orientation, size: 4, points: 4, name: String::from("Battleship")},
-            ShipType::Submarine => Ship { ship_type, origin, orientation, size: 3, points: 6, name: String::from("Submarine")},
-            ShipType::Destroyer => Ship { ship_type, origin, orientation, size: 2, points: 8, name: String::from("Destroyer")},
-            ShipType::PatrolBoat => Ship { ship_type, origin, orientation, size: 1, points: 10, name: String::from("Patrol Boat")},
+            ShipType::AircraftCarrier => Ship { ship_type, origin: None, orientation: None, size: 5, points: 2, name: String::from("Aircraft Carrier")},
+            ShipType::Battleship => Ship { ship_type, origin: None, orientation: None, size: 4, points: 4, name: String::from("Battleship")},
+            ShipType::Submarine => Ship { ship_type, origin: None, orientation: None, size: 3, points: 6, name: String::from("Submarine")},
+            ShipType::Destroyer => Ship { ship_type, origin: None, orientation: None, size: 2, points: 8, name: String::from("Destroyer")},
+            ShipType::PatrolBoat => Ship { ship_type, origin: None, orientation: None, size: 1, points: 10, name: String::from("Patrol Boat")},
         }
     }
 
-    pub fn getPoint(&self) -> i32 {
+    pub fn get_point(&self) -> i32 {
         self.points
     }
 
-    pub fn getSize(&self) -> i32 {
+    pub fn get_size(&self) -> i32 {
         self.size
+    }
+
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+
+    pub fn get_type(&self) -> &ShipType {
+        &self.ship_type
     }
 }
 
@@ -61,8 +71,7 @@ impl Ship {
 
 #[derive(Debug)]
 pub struct Grid {
-    pub layout: [[Square; 10]; 10],
-    offset: i32
+    pub layout: [[Square; 10]; 10]
 }
 
 
