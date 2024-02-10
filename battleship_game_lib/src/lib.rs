cfed808b10ed24e7283f31fd6493b65cb0d6df3e
Adetayo Olaitan <olaitan.adetayo@gmail.com>
2024-02-10 02:13:23 +0000

    made minor changes

--- battleship_game_lib/src/lib.rs
+++ battleship_game_lib/src/lib.rs
@@ -71,6 +71,9 @@ mod tests {
         play.get_session_as_mut().shoot_ship(GridPoint { x: 3 , y:  3});
         play.get_session_as_mut().shoot_ship(GridPoint { x: 4 , y:  4});
         play.get_session_as_mut().shoot_ship(GridPoint { x: 5 , y:  5});
+
+        println!("{:?}", play.get_session_as_ref().get_destroyed_ships());
+        
         play.get_session_as_mut().shoot_ship(GridPoint { x: 6 , y:  6});
         play.get_session_as_mut().shoot_ship(GridPoint { x: 7 , y:  7});
         play.get_session_as_mut().shoot_ship(GridPoint { x: 8 , y:  8});
